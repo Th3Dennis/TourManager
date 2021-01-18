2084e21eda4ee44b1be1798d6f9bf0efad5e6535
Dennis Miceli <dennis.miceli@hotmail.ch>
2021-01-18 11:42:09 +0100

    You can now create custom CardViews. The Main Class will then show them

--- app/src/main/java/ch/th3dennis/tourmanagerdennismiceli/MainActivity.java
+++ app/src/main/java/ch/th3dennis/tourmanagerdennismiceli/MainActivity.java
@@ -53,27 +53,101 @@ public class MainActivity extends AppCompatActivity {
                 openCreateTourActivity();
             }
         });
+    }
 
-
+    @Override
+    protected void onResume() {
+        super.onResume();
 
         //ViewCardHolder
         recyclerView = findViewById(R.id.recyclerview_main);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
         myAdapter = new MyAdapter(this, getMyList());
         recyclerView.setAdapter(myAdapter);
-
 
     }
 
     private ArrayList<CardViewModel> getMyList(){
+        ArrayList<CardViewModel> models = new ArrayList<>();
+
+        List<Tour> tours = tourDao.getAll();
+
+        for (Tour tour : tours){
+            CardViewModel m = new CardViewModel();
+            m.setTitle(tour.getTitle());
+            m.setDescription(tour.getDescription());
+            m.setImg(R.drawable.motorcycle_picture_square);
+            models.add(m);
+        }
+
+        return models;
+    }
+
+
+
+    private ArrayList<CardViewModel> getMyDummyList(){
        ArrayList<CardViewModel> models = new ArrayList<>();
 
        CardViewModel m = new CardViewModel();
        m.setTitle("Test Titel");
        m.setDescription("Dies ist eine Test Beschreibung");
-       m.setImg(R.drawable.motorcycle_picture_1);
+       m.setImg(R.drawable.motorcycle_picture_square);
        models.add(m);
+
+        CardViewModel m1 = new CardViewModel();
+        m1.setTitle("Test Titel1");
+        m1.setDescription("Dies ist eine Test Beschreibung");
+        m1.setImg(R.drawable.motorcycle_picture_square);
+        models.add(m1);
+
+        CardViewModel m2 = new CardViewModel();
+        m2.setTitle("Test Titel2");
+        m2.setDescription("Dies ist eine Test Beschreibung");
+        m2.setImg(R.drawable.motorcycle_picture_square);
+        models.add(m2);
+
+        CardViewModel m3 = new CardViewModel();
+        m3.setTitle("Test Titel3");
+        m3.setDescription("Dies ist eine Test Beschreibung");
+        m3.setImg(R.drawable.motorcycle_picture_square);
+        models.add(m3);
+
+        CardViewModel m4 = new CardViewModel();
+        m4.setTitle("Test Titel4");
+        m4.setDescription("Dies ist eine Test Beschreibung");
+        m4.setImg(R.drawable.motorcycle_picture_square);
+        models.add(m4);
+
+        CardViewModel m5 = new CardViewModel();
+        m5.setTitle("Test Titel5");
+        m5.setDescription("Dies ist eine Test Beschreibung");
+        m5.setImg(R.drawable.motorcycle_picture_square);
+        models.add(m5);
+
+        CardViewModel m6 = new CardViewModel();
+        m6.setTitle("Test Titel6");
+        m6.setDescription("Dies ist eine Test Beschreibung");
+        m6.setImg(R.drawable.motorcycle_picture_square);
+        models.add(m6);
+
+        CardViewModel m7 = new CardViewModel();
+        m7.setTitle("Test Titel7");
+        m7.setDescription("Dies ist eine Test Beschreibung");
+        m7.setImg(R.drawable.motorcycle_picture_square);
+        models.add(m7);
+
+        CardViewModel m8 = new CardViewModel();
+        m8.setTitle("Test Titel8");
+        m8.setDescription("Dies ist eine Test Beschreibung");
+        m8.setImg(R.drawable.motorcycle_picture_square);
+        models.add(m8);
+
+        CardViewModel m9 = new CardViewModel();
+        m9.setTitle("Test Titel9");
+        m9.setDescription("Dies ist eine Test Beschreibung");
+        m9.setImg(R.drawable.motorcycle_picture_square);
+        models.add(m9);
+
 
        return models;
     }
